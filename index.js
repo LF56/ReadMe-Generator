4280e238565b15672682853af62a4573f7cb2bb1
LF56 <leah.fox7@gmail.com>
2022-02-18 11:44:25 -0500

    added more questions to readme generator

--- index.js
+++ index.js
@@ -4,59 +4,84 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'projectName',
-      message: 'What is your project name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your project name!');
-          return false;
-        }
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is your project name? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your project name!');
+        return false;
       }
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is your project description (Required)',
-        validate: descriptionInput => {
-          if (descriptionInput) {
-            return true;
-          } else {
-            console.log('Please enter your description!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'projectInstall',
-        message: 'How do you install your project? (Required)',
-        validate: installInput => {
-          if (installInput) {
-            return true;
-          } else {
-            console.log('Please enter your project name!');
-            return false;
-          }
-        }
-      },
-   
-  ];
+    }
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'What is your project description (Required)',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please enter your description!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'projectInstall',
+    message: 'How do you install your project? (Required)',
+    validate: installInput => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log('Please explain how to install.');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'projectUsage',
+    message: 'How do you use your project? (Required)',
+    validate: installInput => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log('Please explain application use.');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'What license would you like to use for your project?',
+    choices: ['MIT LIcense', 'GPL License', 'Apache License', 'N/A']
+  },
+  {
+    type: 'confirm',
+    name: 'confirmAddContributer',
+    message: 'Would you like to add a contributer to this project?',
+    default: false
+  },
+
+];
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 const init = () => {
-    inquirer.prompt(questions).then(answers => {
-        console.log(answers)
-    }).catch(error =>{
-        console.log(error)
-    })
-  };
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers)
+  }).catch(error => {
+    console.log(error)
+  })
+};
 // Function call to initialize app
 init();
 
